b438962bca3de8cd5e91dcf04cc5a517fe1ddbb0
Sandra Liliana Reyes Castiblanco <sreyesc24@gmail.com>
2023-04-15 15:57:33 -0500

    Pay Mode View

---
+++ Views/IPayModeView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Views
+{
+    internal interface IPayModeView
+    {
+        string PayModeId { get; set; }
+        string PayModeName { get; set; }
+        string PayModeObservation { get; set; }
+        string SearchValue { get; set; }
+        string IsEdit { get; set; }
+        string IsSuccesful { get; set; }
+        string Message { get; set; }
+
+        event EventHandler SearchEvent;
+        event EventHandler AddNewEvent;
+        event EventHandler EditEvent;
+        event EventHandler DeleteEvent;
+        event EventHandler SaveEevent;
+        event EventHandler CancelEvent;
+
+        void SetPayModeListBildingSource(BindingSource payModeList);
+        void Show();
+    }
+}
