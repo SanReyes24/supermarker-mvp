86d012b6a6067ba7bd0b81807cfd6885cdf9ff52
Sandra Liliana Reyes Castiblanco <sreyesc24@gmail.com>
2023-04-18 19:29:35 -0500

    Main View

--- Views/PayModeView.Designer.cs
+++ Views/PayModeView.Designer.cs
@@ -158,6 +158,7 @@
             // 
             DgPayMode.AllowUserToAddRows = false;
             DgPayMode.AllowUserToDeleteRows = false;
+            DgPayMode.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             DgPayMode.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             DgPayMode.Location = new Point(22, 68);
             DgPayMode.Name = "DgPayMode";
