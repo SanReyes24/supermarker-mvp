e966bcf90137bfa0d05ad62b33f6d49710721b2a
Sandra Liliana Reyes Castiblanco <sreyesc24@gmail.com>
2023-04-21 21:12:04 -0500

    CustomerPresenter

---
+++ Presenters/CustomerPresenter.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supermarket_mvp.Views;
+using Supermarket_mvp.Models;
+using static Azure.Core.HttpHeader;
+using System.Drawing;
+
+namespace Supermarket_mvp.Presenters
+{
+    internal class CustomerPresenter
+    {
+        private ICustomerView view;
+        private ICustomerRepository repository;
+        private BindingSource customerBindingSource;
+        private IEnumerable<CustomerModel> customerList;
+
+        public CustomerPresenter(ICustomerView view, ICustomerRepository repository)
+        {
+            this.customerBindingSource = new BindingSource();
+
+            this.view = view;
+            this.repository = repository;
+
+            this.view.SearchEvent += SearchCustomer;
+            this.view.AddNewEvent += AddNewCustomer;
+            this.view.EditEvent += LoadSelectCustomerToEdit;
+            this.view.DeleteEvent += DeleteSelectedCustomer;
+            this.view.SaveEvent += SaveCustomer;
+            this.view.CancelEvent += CancelAction;
+
+            this.view.SetCustomerListBildingSource(customerBindingSource);
+
+            LoadAllCustomerList();
+
+            this.view.Show();
+
+        }
+
+        private void LoadAllCustomerList()
+        {
+            customerList = repository.GetAll();
+            customerBindingSource.DataSource = customerList;
+        }
+
+        private void CancelAction(object? sender, EventArgs e)
+        {
+            CleanViewFields();
+        }
+
+        private void SaveCustomer(object? sender, EventArgs e)
+        {
+            var customer = new CustomerModel();
+            customer.Id = Convert.ToInt32(view.CustomerId);
+            customer.Name = view.CustomerName;
+            customer.Observation = view.CustomerObservation;
+
+            try
+            {
+                new Common.ModelDataValidation().Validate(customer);
+                if (view.IsEdit)
+                {
+                    repository.Edit(customer);
+                    view.Message = "Customer edited successfuly";
+                }
+                else
+                {
+                    repository.Add(customer);
+                    view.Message = "Customer added successfuly";
+                }
+                view.IsSuccesful = true;
+                LoadAllCustomerList();
+                CleanViewFields();
+            }
+            catch (Exception ex)
+            {
+                view.IsSuccesful = false;
+                view.Message = ex.Message;
+            }
+        }
+
+        private void CleanViewFields()
+        {
+            view.CustomerId = "0";
+            view.CustomerName = "";
+            view.CustomerObservation = "";
+        }
+
+        private void DeleteSelectedCustomer(object? sender, EventArgs e)
+        {
+            try
+            {
+                var customer = (CustomerModel)customerBindingSource.Current;
+
+                repository.Delete(customer.Id);
+                view.IsSuccesful = true;
+                view.Message = "Customer deleted successfully";
+                LoadAllCustomerList();
+            }
+            catch (Exception ex)
+            {
+                view.IsSuccesful = false;
+                view.Message = "An error ocurred, could ot delete product";
+            }
+        }
+
+        private void LoadSelectCustomerToEdit(object? sender, EventArgs e)
+        {
+            var customer = (CustomerModel)customerBindingSource.Current;
+
+            view.CustomerId = customer.Id.ToString();
+            view.CustomerName = customer.Name;
+            view.CustomerObservation = customer.Observation;
+
+            view.IsEdit = true;
+        }
+
+        private void AddNewCustomer(object? sender, EventArgs e)
+        {
+            view.IsEdit = false;
+        }
+
+        private void SearchCustomer(object? sender, EventArgs e)
+        {
+            bool emptyValue = string.IsNullOrWhiteSpace(this.view.SearchValue);
+            if (emptyValue == false)
+            {
+                customerList = repository.GetByValue(this.view.SearchValue);
+            }
+            else
+            {
+                customerList = repository.GetAll();
+            }
+            customerBindingSource.DataSource = customerList;
+        }
+    }
+}
