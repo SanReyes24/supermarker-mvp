be2760ec43785ab6ccca61fbbdc803cbd738ab17
Sandra Liliana Reyes Castiblanco <sreyesc24@gmail.com>
2023-04-21 20:36:41 -0500

    ICustomerView

---
+++ Views/ICustomerView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Views
+{
+    internal interface ICustomerView
+    {
+        string CustomerId { get; set; }
+        string CustomerName { get; set; }
+        string CustomerObservation { get; set; }
+        string SearchValue { get; set; }
+        bool IsEdit { get; set; }
+        bool IsSuccesful { get; set; }
+        string Message { get; set; }
+
+        event EventHandler SearchEvent;
+        event EventHandler AddNewEvent;
+        event EventHandler EditEvent;
+        event EventHandler DeleteEvent;
+        event EventHandler SaveEvent;
+        event EventHandler CancelEvent;
+
+        void SetCustomerListBildingSource(BindingSource customerList);
+        void Show();
+    }
+}
