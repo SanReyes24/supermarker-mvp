afd4aafc900b3783cd800f7f83a52f2719408a37
Sandra Liliana Reyes Castiblanco <sreyesc24@gmail.com>
2023-04-21 21:23:42 -0500

    CustomerRepository

---
+++ _Repositories/CustomerRepository.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data;
+using Supermarket_mvp.Models;
+using System.Data;
+
+namespace Supermarket_mvp._Repositories
+{
+    internal class CustomerRepository : BaseRepository, ICustomerRepository
+    {
+        public CustomerRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        public void Add(CustomerModel customerModel)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "INSERT INTO Customer VALUES (@name, @observation)";
+                command.Parameters.Add("@name", SqlDbType.NVarChar).Value = customerModel.Name;
+                command.Parameters.Add("@observation", SqlDbType.NVarChar).Value = customerModel.Observation;
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "DELETE FROM Customer WHERE Customer_Id = @id";
+                command.Parameters.Add("@id", SqlDbType.Int).Value = id;
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Edit(CustomerModel customerModel)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = @"UPDATE Customer
+                                        SET Customer_Name =@name,
+                                        Customer_Observation = @observation
+                                        WHERE Customer_Id = @id";
+                command.Parameters.Add("@name", SqlDbType.NVarChar).Value = customerModel.Name;
+                command.Parameters.Add("@observation", SqlDbType.NVarChar).Value = customerModel.Observation;
+                command.Parameters.Add("@id", SqlDbType.Int).Value = customerModel.Id;
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public IEnumerable<CustomerModel> GetAll()
+        {
+            var customerList = new List<CustomerModel>();
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "SELECT * FROM Customer ORDER BY Customer_Id DESC";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var customerModel = new CustomerModel();
+                        customerModel.Id = (int)reader["Customer_Id"];
+                        customerModel.Name = reader["Customere_Name"].ToString();
+                        customerModel.Observation = reader["Customer_Observation"].ToString();
+                        customerList.Add(customerModel);
+                    }
+                }
+            }
+            return customerList;
+        }
+
+        public IEnumerable<CustomerModel> GetByValue(string value)
+        {
+            var customerList = new List<CustomerModel>();
+            int customerId = int.TryParse(value, out _) ? Convert.ToInt32(value) : 0;
+            string customerName = value;
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = @"SELECT * FROM Customer
+                                        WHERE Customer_Id=@id or Customer_Name LIKE @name+ '%'
+                                        ORDER By Customer_Id DESC";
+                command.Parameters.Add("@id", SqlDbType.Int).Value = customerId;
+                command.Parameters.Add("@name", SqlDbType.NVarChar).Value = customerName;
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var customerModel = new CustomerModel();
+                        customerModel.Id = (int)reader["Customer_Id"];
+                        customerModel.Name = reader["Customer_Name"].ToString();
+                        customerModel.Observation = reader["Customer_Observation"].ToString();
+                        customerList.Add(customerModel);
+                    }
+                }
+
+            }
+            return customerList;
+
+        }
+    
+    }
+}
